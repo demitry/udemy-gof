d4ba4cb29ddb8300c242d7679984514df2c714b6
Dmitry Poluektov <dpoluektov@gmail.com>
2022-11-15 02:01:46 +0200

    Fluent Builder + Fluent Builder Inheritance with Recursive Generics + Update projects to .NET7

---
+++ DesignPatterns/Practice/GenericFunctionalBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice
+{
+    public abstract class GenericFunctionalBuilder<TSubject, TSelf>
+            where TSubject : new()
+            where TSelf : GenericFunctionalBuilder<TSubject, TSelf>
+    {
+        /// <summary>
+        ///  To hold a list fluent actions.
+        /// </summary>
+        private readonly List<Func<TSubject, TSubject>> actions = new List<Func<TSubject, TSubject>>();
+
+        /// <summary>
+        /// Helps in adding functions to a list of action.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public TSelf Do(Action<TSubject> action)
+        {
+            AddActions(action);
+            return (TSelf)this;
+        }
+
+        /// <summary>
+        /// Adds all fuctions to a list
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        private TSelf AddActions(Action<TSubject> action)
+        {
+            actions.Add(p => {
+                action(p);
+                return p;
+            });
+            return (TSelf)this;
+        }
+
+        /// <summary>
+        /// Builds Subject In Very Fluent Way
+        /// </summary>
+        /// <returns></returns>
+        public TSubject Build()
+        {
+            return actions.Aggregate(new TSubject(), (p, f) => f(p));
+        }
+    }
+}
