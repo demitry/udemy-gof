e162f662a79f2e3eecc9b3a2455843a182e6862d
Dmitry Poluektov <dpoluektov@gmail.com>
2022-11-16 04:14:36 +0200

    Functional Builder

---
+++ DesignPatterns/Creational/Builder/FunctionalBuilder/Program.cs
@@ -0,0 +1,44 @@
+﻿using PatternLibrary;
+
+var person = new PersonBuilder()
+    .Called("Sarah")
+    .WorksAs("Developer")
+    .Build();
+
+Console.WriteLine(person);
+
+public class Person
+{
+    public string? Name, Position;
+    public override string ToString() => $"{Name} works as {Position}";
+}
+
+//public sealed class PersonBuilder
+//{
+//    //Mutating functions
+//    private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
+//    public PersonBuilder Called(string name) => Do(p => p.Name = name);
+//    public PersonBuilder Do(Action<Person> action) => AddAction(action);
+//    public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
+//    private PersonBuilder AddAction(Action<Person> action)
+//    {
+//        actions.Add(p =>
+//        {
+//            action(p);
+//            return p;
+//        });
+//        return this;
+//    }
+//} //Let it be more generic -> FunctionalBuilder
+
+public sealed class PersonBuilder: FunctionalBuilder<Person, PersonBuilder>
+{
+    public PersonBuilder Called(string name) => Do(p => p.Name = name);
+}
+
+public static class PersonBuilderExtensions
+{
+    public static PersonBuilder WorksAs
+        (this PersonBuilder builder, string position)
+        => builder.Do(p => p.Position = position);
+}
---
+++ DesignPatterns/PatternLibrary/FunctionalBuilder.cs
@@ -0,0 +1,24 @@
+﻿namespace PatternLibrary
+{
+    public abstract class FunctionalBuilder<TSubject, TSelf>
+        where TSelf : FunctionalBuilder<TSubject, TSelf>
+        where TSubject : new()
+    {
+        private readonly List<Func<TSubject, TSubject>> actions = new List<Func<TSubject, TSubject>>();
+
+        public TSelf Do(Action<TSubject> action) => AddAction(action);
+
+        public TSubject Build() => actions.Aggregate(new TSubject(), (p, f) => f(p));
+
+        private TSelf AddAction(Action<TSubject> action)
+        {
+            actions.Add(p =>
+            {
+                action(p);
+                return p;
+            });
+
+            return (TSelf) this;
+        }
+    }
+}
