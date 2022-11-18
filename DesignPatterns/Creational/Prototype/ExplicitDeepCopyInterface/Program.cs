56889611ca500a05ac70271b5a774aae21284ee9
demitry <dpoluektov@gmail.com>
2022-11-19 01:57:51 +0200

    Explicit Deep Copy Interface

--- DesignPatterns/Creational/Prototype/ExplicitDeepCopyInterface/Program.cs
+++ DesignPatterns/Creational/Prototype/ExplicitDeepCopyInterface/Program.cs
@@ -1,15 +1,32 @@
 ﻿var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
-Console.WriteLine(john);
 
-class Person
+var jane = john.DeepCopy();
+jane.Address.HouseNumber = 1;
+
+Console.WriteLine(john);
+Console.WriteLine(jane);
+
+//But we still stuck with this deep copy 20 modifications.
+
+public interface IPrototype<T>
+{
+    T DeepCopy();
+}
+
+class Person: IPrototype<Person>
 {
     public string[] Names;
-    private Address Address;
+    public Address Address;
 
     public Person(string[] names, Address address)
     {
         Names = names;
         Address = address;
+    }
+
+    public Person DeepCopy()
+    {
+        return new Person(Names, Address.DeepCopy());
     }
 
     public override string ToString()
@@ -18,14 +35,19 @@ class Person
     }
 }
 
-public class Address
+public class Address: IPrototype<Address>
 {
-    private string StreetName;
+    public string StreetName;
     public int HouseNumber;
 
     public override string ToString()
     {
         return $"{StreetName} {HouseNumber}";
+    }
+
+    public Address DeepCopy()
+    {
+        return new Address(StreetName, HouseNumber);
     }
 
     public Address(string streetName, int houseNumber)
