dfa9914959b2ccca7935890c8631ceb0b20ce3f5
demitry <dpoluektov@gmail.com>
2022-11-19 01:21:18 +0200

    Copy Ctro Approach, similar to C++

--- DesignPatterns/Creational/Prototype/CopyConstructor/Program.cs
+++ DesignPatterns/Creational/Prototype/CopyConstructor/Program.cs
@@ -1,15 +1,27 @@
 ﻿var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
 Console.WriteLine(john);
 
+var jane = new Person(john);
+jane.Address.HouseNumber = 321;
+Console.WriteLine(jane);
+
+// Copy Ctor - C++ Approuch, not good for us...
+
 class Person
 {
     public string[] Names;
-    private Address Address;
+    public Address Address;
 
     public Person(string[] names, Address address)
     {
         Names = names;
         Address = address;
+    }
+
+    public Person(Person other)
+    {
+        Names = other.Names;
+        Address = other.Address;
     }
 
     public override string ToString()
@@ -21,16 +33,22 @@ class Person
 public class Address
 {
     private string StreetName;
-    public int HouseBumber;
+    public int HouseNumber;
 
     public override string ToString()
     {
-        return $"{StreetName} {HouseBumber}";
+        return $"{StreetName} {HouseNumber}";
+    }
+
+    public Address(Address other)
+    {
+        StreetName = other.StreetName;
+        HouseNumber = other.HouseNumber;
     }
 
     public Address(string streetName, int houseBumber)
     {
         StreetName = streetName;
-        HouseBumber = houseBumber;
+        HouseNumber = houseBumber;
     }
 }
