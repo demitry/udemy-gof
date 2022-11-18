5c8b778d4cc92008f5e6355075fa37ae3f36bb8f
demitry <dpoluektov@gmail.com>
2022-11-19 00:50:50 +0200

    Why IClonable is bad!

---
+++ DesignPatterns/Creational/Prototype/BadIClonable/Program.cs
@@ -0,0 +1,54 @@
+﻿var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+
+Console.WriteLine(john);
+
+//var jane = john;
+var jane = (Person)john.Clone();
+
+jane.Names[0] = "Jane";
+Console.WriteLine(jane);
+Console.WriteLine(john);
+
+class Person : ICloneable //we don't know is it deep or shalow cloning? shalow cloning! It's bad!
+{
+    public string[] Names;
+    private Address Address;
+
+    public Person(string[] names, Address address)
+    {
+        Names = names;
+        Address = address;
+    }
+
+    public object Clone()
+    {
+        return new Person(Names, (Address) Address.Clone()); //and clone, and clone...
+    }
+
+    public override string ToString()
+    {
+        return $"{string.Join(" ", Names)}, {Address}";
+    }
+}
+
+public class Address: ICloneable
+{
+    private string StreetName;
+    public int HouseBumber;
+    
+    public override string ToString()
+    {
+        return $"{StreetName} {HouseBumber}";
+    }
+
+    public object Clone()
+    {
+        return new Address(StreetName, HouseBumber);
+    }
+
+    public Address(string streetName, int houseBumber)
+    {
+        StreetName = streetName;
+        HouseBumber = houseBumber;
+    }
+}
