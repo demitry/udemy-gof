fd4afae8e4e3ad7b6da45602a7d55ef1896cf14e
demitry <dpoluektov@gmail.com>
2022-11-19 23:38:29 +0200

    Copy Through Serialization - Initial

---
+++ DesignPatterns/Creational/Prototype/CopyThroughSerialization/Program.cs
@@ -0,0 +1,36 @@
+﻿﻿var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+Console.WriteLine(john);
+
+class Person
+{
+    public string[] Names;
+    private Address Address;
+
+    public Person(string[] names, Address address)
+    {
+        Names = names;
+        Address = address;
+    }
+
+    public override string ToString()
+    {
+        return $"{string.Join(" ", Names)}, {Address}";
+    }
+}
+
+public class Address
+{
+    private string StreetName;
+    public int HouseNumber;
+
+    public override string ToString()
+    {
+        return $"{StreetName} {HouseNumber}";
+    }
+
+    public Address(string streetName, int houseBumber)
+    {
+        StreetName = streetName;
+        HouseNumber = houseBumber;
+    }
+}
