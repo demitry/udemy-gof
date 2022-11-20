e92fc35affa44ba84ec3755171659a9ff91c41e5
demitry <dpoluektov@gmail.com>
2022-11-20 23:49:02 +0200

    Monostate Pattern: Static private data fields, public non-static properties

---
+++ DesignPatterns/Creational/Singleton/Monostate/Program.cs
@@ -0,0 +1,32 @@
+﻿var ceo = new CEO();
+ceo.Name = "Adam Smith";
+ceo.Age = 55;
+
+var ceo2 = new CEO();
+Console.WriteLine(ceo2);
+
+// Static private data fields, public non-static properties -
+// we allowed to call c-tor, "re-create" the object but use the shared static data.
+
+public class CEO
+{
+    private static string name;
+    private static int age;
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public int Age
+    {
+        get => age;
+        set => age = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+    }
+}
