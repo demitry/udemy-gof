9452b5c3185eb1f5c687ff777a1692ef7b7e17ef
demitry <dpoluektov@gmail.com>
2022-12-11 20:52:17 +0200

    Exercise Coding Bridge - done

--- DesignPatterns/Structural/Bridge/ExerciseCodingBridge/Program.cs
+++ DesignPatterns/Structural/Bridge/ExerciseCodingBridge/Program.cs
@@ -12,38 +12,93 @@
 
 namespace ExerciseCodingBridge
 {
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        string IRenderer.WhatToRenderAs => "pixels";
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        string IRenderer.WhatToRenderAs => "lines";
+    }
+
     public abstract class Shape
     {
+        protected IRenderer renderer;
+
         public string Name { get; set; } = string.Empty;
+
+        public Shape(IRenderer renderer, string name)
+        {
+            this.renderer = renderer;
+            Name = name;
+        }
+
+        public override string ToString() => $"Drawing {Name} as {renderer.WhatToRenderAs}";
     }
 
     public class Triangle : Shape
     {
-        public Triangle() => Name = "Triangle";
+        public Triangle(IRenderer renderer) : base(renderer, "")
+        {
+            Name = "Triangle";
+        }
     }
 
     public class Square : Shape
     {
-        public Square() => Name = "Square";
+        public Square(IRenderer renderer) : base(renderer, "")
+        {
+            Name = "Square";
+        }
     }
 
     public class VectorSquare : Square
     {
-        public override string ToString() => "Drawing {Name} as lines";
+        public VectorSquare() : base(new VectorRenderer()) {}
     }
 
     public class RasterSquare : Square
     {
-        public override string ToString() => "Drawing {Name} as pixels";
+        public RasterSquare() : base(new RasterRenderer()) {}
     }
 
     // imagine VectorTriangle and RasterTriangle are here too
+    
+    public class VectorTriangle : Triangle
+    {
+        public VectorTriangle() : base(new VectorRenderer()) { }
+    }
+
+    public class RasterTriangle : Triangle
+    {
+        public RasterTriangle() : base(new RasterRenderer()) { }
+    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var triangle = new Triangle(new RasterRenderer()).ToString(); // returns "Drawing Triangle as pixels" 
+
+            Console.WriteLine(triangle);
+
+            var rs = new RasterSquare();
+            Console.WriteLine(rs);
+
+            var vs = new VectorSquare();
+            Console.WriteLine(vs);
+
+            var rt = new RasterTriangle();
+            Console.WriteLine(rt);
+
+            var vt = new VectorTriangle();
+            Console.WriteLine(vt);
         }
     }
 }
